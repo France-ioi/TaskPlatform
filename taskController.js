7ab8805d31736a43a31aeb4632945ef543052c71
eroux <elie.roux@telecom-bretagne.eu>
2015-07-31 15:51:52 +0200

    include submission-manager through bower

--- taskController.js
+++ taskController.js
@@ -1,4 +1,4 @@
-var app = angular.module('pemTask', ['ui.bootstrap']);
+var app = angular.module('pemTask', ['ui.bootstrap','submission-manager']);
 
 angular.module('pemTask')
 .controller('taskController', ['$scope', function($scope) {
