97536fb4f422ed908d4ad0d12c46eb42ef725291
Elie Roux <elie.roux@telecom-bretagne.eu>
2015-08-17 17:23:31 +0200

    add sTaskPath (and other minor improvements=

--- dbv_data/schema/tm_tasks_limits.sql
+++ dbv_data/schema/tm_tasks_limits.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS `tm_tasks_limits` (
   `sLangProg` varchar(255) NOT NULL DEFAULT '*',
   `iMaxTime` int(11) NOT NULL DEFAULT '10000' COMMENT 'max allowed time in ms',
   `iMaxMemory` int(11) NOT NULL COMMENT 'max allowed memory in kb',
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`ID`),
+  KEY `idTask` (`idTask`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
