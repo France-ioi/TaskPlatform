6afbd67648691b92442df838f1d0cc415e852756
Elie Roux <elie.roux@telecom-bretagne.eu>
2015-08-18 19:56:37 +0200

    handling tm_submissions_tests

--- grader/pushResults.php
+++ grader/pushResults.php
@@ -31,6 +31,20 @@ if (!$tokenParams) {
    exit;
 }
 
+// get task ID and name -> ID correspondance for tasks_tests:
+$stmt =$db->prepare('SELECT tm_tasks_tests.idTask, tm_tasks_tests.sName, tm_tasks_tests.ID from tm_tasks_tests 
+   JOIN tm_submissions ON tm_submissions.idTask = tm_tasks_tests.idTask 
+   WHERE tm_submissions.ID = :idSubmission and ((tm_tasks_tests.sGroupType = \'Evaluation\' or tm_tasks_tests.sGroupType = \'Example\') or (tm_tasks_tests.sGroupType = \'User\' and tm_tasks_tests.idUser = tm_submissions.idUser and tm_tasks_tests.idPlatform = tm_submissions.idPlatform));');
+$stmt->execute(array('idSubmission' => $tokenParams['sTaskName']));
+$allTests = $stmt->fetchAll();
+$testsNameID = array();
+$task_id = null;
+
+foreach ($allTests as $test) {
+   $task_id = $test['idTask'];
+   $testsNameID[$test['sName']] = $test['ID'];
+}
+
 $graderResults = $tokenParams['sResultData'];
 
 $nbTestsPassed = 0;
@@ -39,7 +53,10 @@ $nbTestsTotal = 0;
 $bCompilError = false;
 $sErrorMsg = '';
 
-// TODO: not sure about [0] here... summarizeResults uses a for loop
+// TODO: handle subtasks (currently no substask is used)
+
+// there are as many executions as there are sources to evaluate, so here
+// we use only one:
 foreach ($graderResults['executions'][0]['testsReports'] as $testReport) {
    $nbTestsTotal = $nbTestsTotal + 1;
    if (!isset($testReport['checker'])) {
@@ -52,11 +69,22 @@ foreach ($graderResults['executions'][0]['testsReports'] as $testReport) {
       }
       break; // ?
    } else {
-      $iScore = intval(strtok($testReport['checker']['stdout'], "\n")); // TODO: couldn't this be more straightforward?
+      $iScore = intval(strtok($testReport['checker']['stdout']["data"], "\n")); // TODO: make a score field in the json
       if ($iScore > 50) { // TODO: ???
          $nbTestsPassed = $nbTestsPassed + 1;
       }
+      $iTimeMs = $testReport['checker']['timeTakenMs'];
+      $iMemoryKb = $testReport['checker']['memoryUsedKb'];
       $iScoreTotal = $iScoreTotal + $iScore;
+      $idTest = $testsNameID[$testReport['name']];
+      if (!$idTest) {
+         error_log('cannot find test '.$testReport['name'].'for submission '.$tokenParams['sTaskName']);
+         echo json_encode(array('bSuccess' => false, 'sError' => 'cannot find test '.$testReport['name'].'for submission '.$tokenParams['sTaskName']));
+         exit;
+      }
+      // TODO: what is iErrorCode?
+      $stmt = $db->prepare('insert ignore into tm_submissions_tests (idSubmission, idTest, iScore, iTimeMs, iMemoryKb) values (:idSubmission, :idTest, :iScore, :iTimeMs, :iMemoryKb);');
+      $stmt->execute(array('idSubmission' => $tokenParams['sTaskName'], 'idTest' => $idTest, 'iScore' => $iScore, 'iTimeMs' => $iTimeMs, 'iMemoryKb' => $iMemoryKb));
    }
 }
 
