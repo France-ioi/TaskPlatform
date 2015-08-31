cda4f60a0ac069c3334df62167e1bc1600807f81
Sebastien Carlier <s.carlier@epixode.fr>
2015-08-31 12:19:32 +0000

    fix history_tm_tasks_strings schema

--- dbv_data/schema/history_tm_tasks_strings.sql
+++ dbv_data/schema/history_tm_tasks_strings.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `tm_tasks_strings` (
+CREATE TABLE IF NOT EXISTS `history_tm_tasks_strings` (
   `historyID` bigint(20) NOT NULL AUTO_INCREMENT,
   `ID` bigint(20) NOT NULL,
   `idTask` bigint(20) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `tm_tasks_strings` (
   `iVersion` int(11) NOT NULL DEFAULT '0',
   `iNextVersion` int(11) DEFAULT NULL,
   `bDeleted` tinyint(1) NOT NULL,
-  PRIMARY KEY (`ID`),
+  PRIMARY KEY (`historyID`),
   KEY (`idTask`),
   KEY `idTasksLang` (`idTask`, `sLanguage`),
   KEY `iVersion` (`iVersion`),
