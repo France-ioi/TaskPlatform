bf94e0dae7920f017afb68ee7241e609aca4b43c
Elie Roux <elie.roux@telecom-bretagne.eu>
2015-09-03 17:32:18 +0200

    small improvement on tm_solutions schema

--- dbv_data/schema/tm_solutions.sql
+++ dbv_data/schema/tm_solutions.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `tm_solutions` (
   `ID` bigint(20) NOT NULL,
   `idTask` bigint(20) NOT NULL,
-  `bInSolution` tinyint(1) NOT NULL DEFAULT 0,
+  `bInSolution` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'solution is in the solution part of the task',
   `sLangProg` varchar(10) NOT NULL,
-  `sGroup` varchar(50) DEFAULT NULL,
+  `sGroup` varchar(50) NOT NULL,
   `sAuthor` varchar(50) NOT NULL,
   `iVersion` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
