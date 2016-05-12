6cb73951bdcfdde3f3ce394f1bebdbe650f435c0
Elie Roux <elie.roux@telecom-bretagne.eu>
2016-05-12 11:42:18 +0200

    sSolution can probably be null

---
+++ dbv_data/revisions/108/sSolution.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `tm_tasks_strings` CHANGE `sSolution` `sSolution` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+ALTER TABLE `history_tm_tasks_strings` CHANGE `sSolution` `sSolution` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
