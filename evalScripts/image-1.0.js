f524e124015cc92acef460e129bf2695d56d08f7
Elie Roux <elie.roux@telecom-bretagne.eu>
2016-05-25 12:20:53 +0200

    add image eval script

---
+++ evalScripts/image-1.0.js
@@ -0,0 +1,14 @@
+define(function () {
+    return {
+    	displayChecker: function(test) { // tm_submission_test
+    		if (!test.jFiles) {return null;}
+    		if (!test.files) {
+    			test.files = JSON.parse(test.jFiles);
+    		}
+    		if (!test.files || !test.files.length) {return null;}
+		    var imgData = test.files[0].data;
+			imgData = imgData.replace(/(\n|\r)/g, " ");
+			return 'Le résultat de votre programme est : <br><img src="data:image/png;base64,'+ imgData+'">';
+    	}
+    }
+});
