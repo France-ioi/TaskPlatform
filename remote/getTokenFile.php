a7c78d3be5055032973571a0e4c8a3ef56c0ca13
Elie Roux <elie.roux@telecom-bretagne.eu>
2016-06-20 18:54:24 +0200

    generate token file

---
+++ remote/getTokenFile.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once "../shared/connect.php";
+require_once "../shared/TokenGenerator.php";
+require_once "../shared/common.inc.php";
+
+$request = $_GET;
+
+if ((!isset($request['sToken']) && !$config->testMode->active) || !isset($request['sPlatform'])) {
+   echo json_encode(array('bSuccess' => false, 'sError' => 'missing sToken or sPlatform POST variable.'));
+   exit;
+}
+
+$params = getPlatformTokenParams($request['sToken'], $request['sPlatform'], $request['taskId'], $db);
+
+function getRandomID() {
+   $rand = (string) mt_rand(100000, 999999999);
+   $rand .= (string) mt_rand(1000000, 999999999);
+   return $rand;
+}
+
+function getNewSecret($idUser, $idPlatform) {
+	global $db;
+	$newSecret = (string) mt_rand(100000, 999999999);
+	$newSecret .= (string) mt_rand(1000000, 999999999);
+	$stmt = $db->prepare('insert ignore into tm_remote_secret (idUser, idPlatform, sRemoteSecret) values (:idUser, :idPlatform, :secret) on duplicate key update sRemoteSecret = VALUES(sRemoteSecret);');
+	$stmt->execute([
+		'idUser' => $idUser,
+		'idPlatform' => $idPlatform,
+		'secret' => $newSecret
+	]);
+	return $newSecret;
+}
+
+$newSecret = getNewSecret($params['idUser'], $request['sPlatform']);
+
+$newParams = [
+  'idItem' => $params['idItem'],
+  'itemUrl' => $params['itemUrl'],
+  'idUser' => $params['idUser'],
+  'type' => 'long',
+  'secret' => $newSecret
+];
+
+$tokenGenerator = new TokenGenerator($config->platform->private_key, $config->platform->name, null);
+
+$jws = $tokenGenerator->encodeJWS($newParams);
+
+$res = json_encode([
+	'token' => $jws, 
+	'platform' => $request['sPlatform'],
+	'taskId' => $request['taskId'],
+	'baseUrl' => $config->baseUrl
+	]);
+
+header('Content-Type: application/json');
+header("Content-Disposition: attachment; filename=fioi-remote.json");
+header("Content-Length: " . strlen($res));
+
+echo $res;
