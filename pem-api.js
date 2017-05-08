4ea385d86f3266b91414d43bd0b387944eebfe66
Michel Blockelet <blockelet@crans.org>
2017-05-09 01:47:21 +0200

    Add dualViews: show side-by-side the statement and the editor
    If the width is large enough, both views will be shown side-by-side. Platforms
    using tasks from TaskPlatform must call again getViews after an iframe resize.

--- pem-api.js
+++ pem-api.js
@@ -13,6 +13,10 @@ var self = this;
 
 this.task = {};
 this.platform = window.platform;
+var dualViewStatus = {
+   loaded: false,
+   enabled: false,
+   active: false};
 
 this.task.showViews = function(viewsToShow, success, error) {
    var requiredViews = {};
@@ -34,6 +38,9 @@ this.task.showViews = function(viewsToShow, success, error) {
          requiredViews[view] = true;
       });
    });
+
+   self.updateEditorInStatement((requiredViews['task'] || requiredViews['editor']) ? 'activate' : 'deactivate');
+
    _.forEach(taskViews, function(params, view) {
       if (requiredViews[view]) {  
          $('#'+view).show();
@@ -64,15 +71,107 @@ this.task.load = function(views, success, error) {
    success();
 };
 
+this.updateEditorInStatement = function(operation) {
+   // Put the editor aside the statement in a dual view pane
+   // operation: one of
+   // -enable: next time task or editor is shown, they will be in a dual view
+   // -disable: disable dual view
+   // -activate: show dual view and show task and editor views if enabled
+   // -deactivate: hide dual view
+
+   if(operation == 'enable') {
+      if(dualViewStatus.enabled) {
+         return;
+      } else {
+         dualViewStatus.enabled = true;
+      }
+   } else if(operation == 'disable') {
+      if(!dualViewStatus.enabled) {
+         return;
+      } else {
+         dualViewStatus.enabled = false;
+      }
+   } else if(operation == 'activate') {
+      if(dualViewStatus.active) {
+         return;
+      } else {
+         dualViewStatus.active = true;
+      }
+   } else if(operation == 'deactivate') {
+      if(!dualViewStatus.active) {
+         return;
+      } else {
+         dualViewStatus.active = false;
+      }
+   }
+
+   if(dualViewStatus.enabled) {
+      taskViews.task = {includes: ["editor", "submission"]};
+      taskViews.editor = {requires: "task"};
+   } else {
+      taskViews.task = {};
+      taskViews.editor = {includes: ["submission"]};
+   }
+
+   var moveViews = function () {
+      if (dualViewStatus.enabled) {
+         // Create dual view
+         if(!$('#dualView').length) {
+            $('body').prepend('<div id="dualView"><div id="dualViewLeft"><div id="dualViewLeftContent"></div></div><div id="dualViewRight"><div id="dualViewRightContent"></div></div></div>');
+         }
+         $('body').width('100%');
+         $('#dualView').show();
+   
+         // Move views in the dualView
+         $('#task').appendTo('#dualViewLeftContent');
+         $('#editor').appendTo('#dualViewRightContent');
+         $('#submission').appendTo('#dualViewRightContent');
+         if(dualViewStatus.active) {
+            $('#task').show();
+            $('#editor').show();
+            $('#submission').show();
+         } else {
+            $('#task').hide();
+            $('#editor').hide();
+            $('#submission').hide();
+         }
+      } else {
+         // Move views out of the dualView
+         $('#submission').prependTo('body').hide();
+         $('#editor').prependTo('body').hide();
+         $('#task').prependTo('body');
+         $('#dualView').hide();
+         $('body').width('772px');
+         $('#task').show();
+      }
+   };
+
+   if(dualViewStatus.enabled && dualViewStatus.active) {
+      if(dualViewStatus.loaded) {
+         moveViews();
+      } else {
+         // On first load, let all initialization happen beforehand
+         setTimeout(moveViews, 10);
+      }
+   } else if(dualViewStatus.active) {
+      moveViews();
+   } else {
+      $('#dualView').hide();
+      $('#task').hide();
+      $('#editor').hide();
+      $('#submission').hide();
+   }
+};
+
 this.updateViews = function() {
    var task_strings = ModelsManager.getRecords('tm_tasks_strings');
    var viewsNeedUpdate = false;
-   for (var stringId in task_strings) {
+   for (var taskID in task_strings) {
       // Add the solution view
-      if (task_strings[stringId].sSolution && (!taskViews.solution || !taskViews.solution.requires)) {
+      if (task_strings[taskID].sSolution && (!taskViews.solution || !taskViews.solution.requires)) {
          taskViews.solution = {};
          viewsNeedUpdate = true;
-      } else if (task_strings[stringId].sSolution && taskViews.solution && taskViews.solution.requires) {
+      } else if (task_strings[taskID].sSolution && taskViews.solution && taskViews.solution.requires) {
          taskViews.solution = {requires: 'task'};
          viewsNeedUpdate = true;
       }
@@ -80,19 +179,19 @@ this.updateViews = function() {
    // add or remove hints view
    taskViews.hints = {requires: 'task'};
    var tasks = ModelsManager.getRecords('tm_tasks');
+   var editorInStatement = (window.innerWidth >= 1574);
    for (var taskID in tasks) {// there should be only one!
       if (tasks[taskID].nbHintsTotal) {
          taskViews.hints = {};
       }
-      if (tasks[taskID].bEditorInStatement) {
-         taskViews.task = {includes: ["editor", "submission"]};
-         taskViews.editor = {requires:"task"};
-      } else {
-         taskViews.task = {};
-         taskViews.editor = {includes: ["submission"]};
-      }
+      editorInStatement = editorInStatement || tasks[taskID].bEditorInStatement;
    }
+
+   // set up statement/editor views
+   self.updateEditorInStatement(editorInStatement ? 'enable' : 'disable');
 };
+
+$(window).resize(this.updateViews.bind(this));
 
 this.task.getViews = function(success, error) {
    try {
@@ -144,7 +243,7 @@ this.task.getState = function(success, error) {
 
 this.task.getMetaData = function(success, error) {
    // TODO: complete
-   success({nbHints:0, minWidth:765});
+   success({nbHints:0, minWidth: 'auto'});
 };
 
 this.task.reloadAnswer = function(strAnswer, success, error) {
