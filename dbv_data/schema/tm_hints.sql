1cb6490e06b4c408d7d6310c87d21d20ac1ffa53
Michel Blockelet <blockelet@crans.org>
2017-11-27 16:52:38 +0100

    Transition to the new SQL schema revision system
    If you were up-to-date before this commit, just execute
        php commonFramework/schema-migrate.php start 1.1/revision-001
        php commonFramework/schema-migrate.php
    to initialize the migration system.

--- dbv_data/schema/tm_hints.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `tm_hints` (
-  `ID` bigint(20) NOT NULL,
-  `idTask` bigint(20) NOT NULL,
-  `iRank` tinyint(11) NOT NULL,
-  `iVersion` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ID`),
-  KEY (`idTask`),
-  KEY `synchro` (`iVersion`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- dbv_data/schema/tm_platforms.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `tm_platforms` (
-  `ID` bigint(20) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `public_key` varchar(500) NOT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY(`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
