7c37c8040f1d2e0ba767060e9d6ef19f213aa6a6
Michel Blockelet <blockelet@crans.org>
2018-07-11 00:04:40 +0200

    Optimize editors saving process to avoid deleting and reinserting all the time

--- loadHistory.php
+++ loadHistory.php
@@ -12,43 +12,6 @@ if (!$config->testMode->active && (!isset($request['sToken']) || !isset($request
 
 $params = getPlatformTokenParams($request['sToken'], $request['sPlatform'], $request['taskId'], $db);
 
-function saveSources($params, $sources, $db) {
-   $db->exec('delete from tm_source_codes where idUser = '.$db->quote($params['idUser']).' and idTask = '.$db->quote($params['idTaskLocal']).' and idPlatform = '.$db->quote($params['idPlatform']).' and bSubmission = \'0\';');
-   if (!count($sources))
-      return;
-   $query = 'insert into tm_source_codes (idUser, idTask, idPlatform, sDate, sName, sSource, sParams, sType, bActive, iRank) values';
-   $rows = array();
-   $iRank = 0;
-   foreach($sources as $source) {
-      $iRank = $iRank + 1;
-      $sourceParams = json_encode_safe(array('sLangProg' => $source['sLangProg']));
-      $bActive = $source['bActive'] ? 1 : 0;
-      $rows[] = '('.$db->quote($params['idUser']).', '.$db->quote($params['idTaskLocal']).', '.$db->quote($params['idPlatform']).', NOW(), '.$db->quote($source['sName']).', '.$db->quote($source['sSource']).', '.$db->quote($sourceParams).', \'User\', '.$bActive.', '.$iRank.')';
-   }
-   $query .= implode(', ', $rows);
-   $db->exec($query);
-}
-
-function saveTests($params, $tests, $db) {
-      $db->exec('delete from tm_tasks_tests where sGroupType = \'User\' and idUser = '.$db->quote($params['idUser']).' and idTask = '.$db->quote($params['idTaskLocal']).' and idPlatform = '.$db->quote($params['idPlatform']));
-      if (!count($tests))
-         return;
-      $query = 'insert into tm_tasks_tests (idUser, idTask, idPlatform, sName, iRank, bActive, sGroupType, sInput, sOutput) values';
-      $rows = array();
-      // rank 0 is for example
-      $iRank = 0;
-      foreach($tests as $test) {
-         $iRank = $iRank + 1;
-         $bActive = $test['bActive'] ? 1 : 0;
-         if (0 === strpos($test['sName'], 'Example')) continue; // example tests
-         $rows[] = '('.$db->quote($params['idUser']).', '.$db->quote($params['idTaskLocal']).', '.$db->quote($params['idPlatform']).', '.$db->quote($test['sName']).', '.$iRank.', '.$db->quote($test['bActive']).', \'User\', '.$db->quote($test['sInput']).', '.$db->quote($test['sOutput']).')';
-      }
-      if (!count($rows))
-         return;
-      $query .= implode(', ', $rows);
-      $db->exec($query);
-}
-
 function listItems($params, $db) {
    $stmt = $db->prepare("
       SELECT history_tm_source_codes.*, tm_submissions.iScore
--- saveEditors.php
+++ saveEditors.php
@@ -18,45 +18,102 @@ if (!isset($request['aSources'])) {
 $params = getPlatformTokenParams($request['sToken'], $request['sPlatform'], $request['taskId'], $db);
 
 function saveSources($params, $sources, $db) {
-   $db->exec('delete from tm_source_codes where idUser = '.$db->quote($params['idUser']).' and idTask = '.$db->quote($params['idTaskLocal']).' and idPlatform = '.$db->quote($params['idPlatform']).' and bSubmission = \'0\';');
-   if (!count($sources))
-      return;
-   $query = 'insert into tm_source_codes (idUser, idTask, idPlatform, sDate, sName, sSource, sParams, sType, bActive, iRank) values';
-   $rows = array();
+   // Insert / update sources
+   $stmt = $db->prepare("SELECT * FROM tm_source_codes WHERE idUser = :idUser AND idTask = :idTask AND idPlatform = :idPlatform AND bSubmission = '0' AND sType = 'User' ORDER BY iRank ASC;");
+   $stmt->execute([
+      'idUser' => $params['idUser'],
+      'idTask' => $params['idTaskLocal'],
+      'idPlatform' => $params['idPlatform']]);
+
    $iRank = 0;
    foreach($sources as $source) {
-      $iRank = $iRank + 1;
+      $oldSource = $stmt->fetch();
       $sourceParams = json_encode_safe(array('sLangProg' => $source['sLangProg']));
       $bActive = $source['bActive'] ? 1 : 0;
-      $rows[] = '('.$db->quote($params['idUser']).', '.$db->quote($params['idTaskLocal']).', '.$db->quote($params['idPlatform']).', NOW(), '.$db->quote($source['sName']).', '.$db->quote($source['sSource']).', '.$db->quote($sourceParams).', \'User\', '.$bActive.', '.$iRank.')';
+
+      if($oldSource && ($source['sName'] != $oldSource['sName'] || $source['sSource'] != $oldSource['sSource'] || $sourceParams != $oldSource['sParams'] || $bActive != $oldSource['bActive'] || $iRank != $oldSource['iRank'])) {
+         $stmt2 = $db->prepare("UPDATE tm_source_codes SET sDate = NOW(), sName = :sName, sSource = :sSource, sParams = :sParams, bActive = :bActive, iRank = :iRank WHERE ID = :id;");
+         $stmt2->execute([
+            'sName' => $source['sName'],
+            'sSource' => $source['sSource'],
+            'sParams' => $sourceParams,
+            'bActive' => $bActive,
+            'iRank' => $iRank,
+            'id' => $oldSource['ID']]);
+      } elseif(!$oldSource) {
+         $stmt2 = $db->prepare("INSERT INTO tm_source_codes (idUser, idTask, idPlatform, sDate, sName, sSource, sParams, bSubmission, sType, bActive, iRank) VALUES(:idUser, :idTask, :idPlatform, NOW(), :sName, :sSource, :sParams, 0, 'User', :bActive, :iRank);");
+         $stmt2->execute([
+            'idUser' => $params['idUser'],
+            'idTask' => $params['idTaskLocal'],
+            'idPlatform' => $params['idPlatform'],
+            'sName' => $source['sName'],
+            'sSource' => $source['sSource'],
+            'sParams' => $sourceParams,
+            'bActive' => $bActive,
+            'iRank' => $iRank]);
+      }
+      $iRank += 1;
    }
-   $query .= implode(', ', $rows);
-   $db->exec($query);
+
+   // Delete lines which weren't inserted/updated
+   $stmt = $db->prepare("DELETE FROM tm_source_codes WHERE idUser = :idUser AND idTask = :idTask AND idPlatform = :idPlatform AND iRank >= :iRank AND bSubmission = '0' AND sType = 'User';");
+   $stmt->execute([
+      'idUser' => $params['idUser'],
+      'idTask' => $params['idTaskLocal'],
+      'idPlatform' => $params['idPlatform'],
+      'iRank' => $iRank]);
 }
 
 function saveTests($params, $tests, $db) {
-      $db->exec('delete from tm_tasks_tests where sGroupType = \'User\' and idUser = '.$db->quote($params['idUser']).' and idTask = '.$db->quote($params['idTaskLocal']).' and idPlatform = '.$db->quote($params['idPlatform']));
-      if (!count($tests))
-         return;
-      $query = 'insert into tm_tasks_tests (idUser, idTask, idPlatform, sName, iRank, bActive, sGroupType, sInput, sOutput) values';
-      $rows = array();
-      // rank 0 is for example
-      $iRank = 0;
-      foreach($tests as $test) {
-         $iRank = $iRank + 1;
-         $bActive = $test['bActive'] ? 1 : 0;
-         if (0 === strpos($test['sName'], 'Example')) continue; // example tests
-         $rows[] = '('.$db->quote($params['idUser']).', '.$db->quote($params['idTaskLocal']).', '.$db->quote($params['idPlatform']).', '.$db->quote($test['sName']).', '.$iRank.', '.$db->quote($test['bActive']).', \'User\', '.$db->quote($test['sInput']).', '.$db->quote($test['sOutput']).')';
+   // Insert / update user tests
+   $stmt = $db->prepare("SELECT * FROM tm_tasks_tests WHERE idUser = :idUser AND idTask = :idTask AND idPlatform = :idPlatform AND sGroupType = 'User' ORDER BY iRank ASC;");
+   $stmt->execute([
+      'idUser' => $params['idUser'],
+      'idTask' => $params['idTaskLocal'],
+      'idPlatform' => $params['idPlatform']]);
+
+   $iRank = 0;
+   foreach($tests as $test) {
+      // Don't save example tests
+      if(0 === strpos($test['sName'], 'Example')) continue;
+
+      $oldTest = $stmt->fetch();
+      $bActive = $test['bActive'] ? 1 : 0;
+
+      if($oldTest && ($test['sName'] != $oldTest['sName'] || $test['sInput'] != $oldTest['sInput'] || $test['sOutput'] != $oldTest['sOutput'] || $bActive != $oldTest['bActive'] || $iRank != $oldTest['iRank'])) {
+         $stmt2 = $db->prepare("UPDATE tm_tasks_tests SET sName = :sName, sInput = :sInput, sOutput = :sOutput, bActive = :bActive, iRank = :iRank WHERE ID = :id;");
+         $stmt2->execute([
+            'sName' => $test['sName'],
+            'sInput' => $test['sInput'],
+            'sOutput' => $test['sOutput'],
+            'bActive' => $bActive,
+            'iRank' => $iRank,
+            'id' => $oldTest['ID']]);
+      } elseif(!$oldTest) {
+         $stmt2 = $db->prepare("INSERT INTO tm_tasks_tests (idUser, idTask, idPlatform, sName, sInput, sOutput, sGroupType, bActive, iRank) VALUES(:idUser, :idTask, :idPlatform, :sName, :sInput, :sOutput, 'User', :bActive, :iRank);");
+         $stmt2->execute([
+            'idUser' => $params['idUser'],
+            'idTask' => $params['idTaskLocal'],
+            'idPlatform' => $params['idPlatform'],
+            'sName' => $test['sName'],
+            'sInput' => $test['sInput'],
+            'sOutput' => $test['sOutput'],
+            'bActive' => $bActive,
+            'iRank' => $iRank]);
       }
-      if (!count($rows))
-         return;
-      $query .= implode(', ', $rows);
-      $db->exec($query);
+      $iRank += 1;
+   }
+
+   // Delete lines which weren't inserted/updated
+   $stmt = $db->prepare("DELETE FROM tm_tasks_tests WHERE idUser = :idUser AND idTask = :idTask AND idPlatform = :idPlatform AND iRank >= :iRank AND sGroupType = 'User';");
+   $stmt->execute([
+      'idUser' => $params['idUser'],
+      'idTask' => $params['idTaskLocal'],
+      'idPlatform' => $params['idPlatform'],
+      'iRank' => $iRank]);
 }
 
 
 saveSources($params, $request['aSources'], $db);
-if (count($request['aTests'])) {
-   saveTests($params, $request['aTests'], $db);
-}
+saveTests($params, $request['aTests'], $db);
 echo json_encode(array('bSuccess' => true));
