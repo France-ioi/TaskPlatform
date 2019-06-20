8a57e92ad198cb15c8bcaaa9667da4d995936e7f
Michel Blockelet <blockelet@crans.org>
2019-06-21 00:12:38 +0200

    quickAlgo: Add additional message on success

--- quickAlgo/taskPlatformAdapter.js
+++ quickAlgo/taskPlatformAdapter.js
@@ -44,6 +44,7 @@ $(function () {
         setTimeout(function() {
             task.displayedSubTask.getGrade(function(results) {
                 if(results.successRate >= 1) {
+                    $('#success').show();
                     window.parent.adapterApi.displayPopup();
                 }
                 task.displayedSubTask.run();
